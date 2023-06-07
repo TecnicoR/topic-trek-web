225b14f889528ed8110692bf54f8b466ac541850
Rabi <rabinarayan.patra@thoughtclan.com>
2023-06-08 03:32:46 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
+import '@coreui/coreui/dist/css/coreui.min.css'
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from "@coreui/react";
+import { Box, Container } from "@mui/material";
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Container>
+        <CCarousel controls indicators>
+          <CCarouselItem>
+            <CImage className="d-block w-100" src={"https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"} alt="slide 1" />
+            <CCarouselCaption className="d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </CCarouselCaption>
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage className="d-block w-100" src={"https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"} alt="slide 2" />
+            <CCarouselCaption className="d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </CCarouselCaption>
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage className="d-block w-100" src={"https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"} alt="slide 3" />
+            <CCarouselCaption className="d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </CCarouselCaption>
+          </CCarouselItem>
+        </CCarousel>
+      </Container>
+    </>
+  );
+};
