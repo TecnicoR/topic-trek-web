046d0f3e6305be75d0f8ce57090ef63876b0c2ac
Rabi <rabinarayan.patra@thoughtclan.com>
2023-06-08 03:19:09 +0530

    created footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import { Navbar } from './components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,36 @@ import {
   AppBar,
   Box,
   IconButton,
+  Menu,
+  MenuItem,
   Stack,
-  Toolbar,
   Tooltip,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import BookIcon from "@mui/icons-material/Book";
 import HomeIcon from "@mui/icons-material/Home";
 import GradeIcon from "@mui/icons-material/Grade";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import PostAddIcon from '@mui/icons-material/PostAdd';
+import PostAddIcon from "@mui/icons-material/PostAdd";
 
 const menuIconStyle = { color: "white", fontSize: "30px" };
 
 export const Navbar = () => {
+  // State used for opening my profile sub menu once clicked on profile icon
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = anchorEl;
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar
-      position="static"
+      position="sticky"
       sx={{
         height: "60px",
         display: "flex",
@@ -71,14 +83,39 @@ export const Navbar = () => {
           </Tooltip>
           <Tooltip title="Add new post">
             <IconButton>
-                <PostAddIcon sx={menuIconStyle}/>
+              <PostAddIcon sx={menuIconStyle} />
             </IconButton>
           </Tooltip>
           <Tooltip title="My Profile">
-            <IconButton >
+            <IconButton
+              aria-controls={open ? "resources-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
               <AccountCircleIcon sx={menuIconStyle} />
             </IconButton>
           </Tooltip>
+          <Menu
+            id="resources-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            MenuListProps={{
+              "aria-labelledby": "resources-button",
+            }}
+          >
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>Logout</MenuItem>
+          </Menu>
         </Box>
       </Stack>
     </AppBar>
