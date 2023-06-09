48f24d7756791101e544cacff049d41dcce5ecb9
Rabi <rabinarayan.patra@thoughtclan.com>
2023-06-09 19:26:16 +0530

    resolvedwarnings

--- src/components/security/ProtectedRoute.jsx
+++ src/components/security/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
-import { Navigate, Route, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }) => {
   const { user } = useContext(UserContext);
